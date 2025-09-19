e9a3c71b37bff9c64d5f2d5312d01b0d6431fb47
Rohan Timalsina <timalsinarohan9@gmail.com>
2025-09-19 14:25:17 +0545

    .

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -6,7 +6,7 @@ import { yellowImg } from "../utils";
 import * as THREE from "three";
 import { Canvas } from "@react-three/fiber";
 import { View } from "@react-three/drei";
-import { models } from "../constants";
+import { models, sizes } from "../constants";
 
 const Model = () => {
     const [size, setsize] = useState("small");
@@ -69,7 +69,7 @@ const Model = () => {
                             }}
                             eventSource={document.getElementById("root")}
                         >
-                            <View.port />
+                            <View.Port />
                         </Canvas>
                     </div>
                     <div className="mx-auto w-full">
@@ -81,7 +81,7 @@ const Model = () => {
                                 {models.map((e, i) => (
                                     <li
                                         key={i}
-                                        className="w-6 h-6 rounded-full mx-2"
+                                        className="w-6 cursor-pointer h-6 rounded-full mx-2"
                                         style={{ backgroundColor: e.color[0] }}
                                         onClick={() => {
                                             setmodel(e);
@@ -89,6 +89,14 @@ const Model = () => {
                                     ></li>
                                 ))}
                             </ul>
+                            <button className="size-btn-container">{sizes.map(({ label, value }) => (
+                                <span className="size-btn" style={{
+                                    backgroundColor: size == value ? 'white' : 'transparent',
+                                    color: size === value ? 'black' : 'white'
+                                }} key={label} onClick={() => setsize(value)
+                                }> {label}</span>
+                            )
+                            )}</button>
                         </div>
                     </div>
                 </div>
--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { PerspectiveCamera, View } from "@react-three/drei";
+import { AmbientLight } from "three";
 
-const ModelView = () => {
+
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, item, size, }) => {
+
+
   return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default ModelView
+    <>
+
+      <View index={index}
+        id={gsapType} className={`border-2 border-red-500 w-full h-full ${index === 2}?'right-[-100%]':''`} >
+        {/* ambientlight */}
+        <ambientLight intensity={.3} />
+        <PerspectiveCamera makeDefault position={ [0,0,4]} />
+      </View >
+    </>
+
+  )
+};
+
+export default ModelView;
